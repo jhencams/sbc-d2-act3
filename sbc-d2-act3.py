ced9ccb7a4bb02fc1c89c972575aa9296f0da8e9
jhencams <zhenboylescamance@gmail.com>
2024-06-19 19:50:25 -0700

    sbc-d2-act3.py

--- sbc-d2-act3.py
+++ sbc-d2-act3.py
@@ -1,31 +1,25 @@
 from random import randint
 
-print("Lottery House")
+print("lottery")
+N1 = int(input("N1 (0-9): "))
+N2 = int(input("N2 (0-9): "))
+N3 = int(input("N3 (0-9): "))
 
-# place your bet number
-bet1 = int(input("Enter 1st number (0-9): "))
-bet2 = int(input("Enter 2nd number (0-9): "))
-bet3 = int(input("Enter 3rd number (0-9): "))
+print("Bet numbers are; ", N1, N2, N3)
 
-
-print("Bet numbers are; ", bet1, bet2, bet3)
-
-# random winning numbers
 r1 = randint(0,9)
 r2 = randint(0,9)
 r3 = randint(0,9)
 
+print("winning numbers are; ", r1, r2, r3)    
 
-print("winning numbers are; ", r1, r2, r3)
-      
-# the result
-if bet1 == r1 and bet2 == r2 and bet3 == r3:
+if N1 == r1 and N2 == r2 and N3 == r3:
     print("YOU WIN!!")
-elif (bet1 == r1 and bet2 == r3 and bet3 == r2) or \
-     (bet1 == r2 and bet2 == r1 and bet3 == r3) or \
-     (bet1 == r2 and bet2 == r3 and bet3 == r1) or \
-     (bet1 == r3 and bet2 == r1 and bet3 == r2) or \
-     (bet1 == r3 and bet2 == r2 and bet3 == r1):
-    print("Almost got your LLUCK!")
+elif (N1 == r1 and N2 == r3 and N3 == r2) or \
+     (N1 == r2 and N2 == r1 and N3 == r3) or \
+     (N1 == r2 and N2 == r3 and N3 == r1) or \
+     (N1 == r3 and N2 == r1 and N3 == r2) or \
+     (N1 == r3 and N2 == r2 and N3 == r1):
+    print("Almost got your LUCK!")
 else:
-    print("THANK YOU!!")
+    print("BETTER LUCK NEXT TIME!!")
